c2deaa078fa125cf6d4ba2bfbc485f5f461a64a6
ketan-5000 <ketan3217.beai24@chitkara.edu.in>
2025-04-18 19:59:25 +0530

    Initial clean upload with .gitignore and requirements.txt

--- BloodDonation/DashboardApp/views.py
+++ BloodDonation/DashboardApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Sum
-from .models import BloodDonation, BloodRequest
+from .models import BloodDonation
 
 
 def is_superuser(user):
